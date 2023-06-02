f400982f4de3c35f3259b04ad4d438294a826346
dean <dean.caloracan@gmail.com>
2023-06-02 17:12:26 +0800

    initial

---
+++ src/app/cards.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+interface CardProps {
+  imageSrc: string;
+  title: string;
+  subtitle: string;
+  link: string;
+}
+
+const Card: FC<CardProps> = ({ imageSrc, title, subtitle, link }) => {
+  return (
+    <a href={link !== '#' ? link : undefined} target="_blank" rel="noopener noreferrer">
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden w-80 md:w-96 mx-auto">
+        <div className="relative">
+          <img src={imageSrc} alt={title} className="h-48 w-full object-cover" />
+          {link !== '#' && (
+            <div className="absolute bottom-0 right-0 p-2">
+              <FiArrowRight size={24} color="white" />
+            </div>
+          )}
+        </div>
+        <div className="p-4">
+          <h3 className="text-xl font-bold mb-2">{title}</h3>
+          <p className="text-gray-600">{subtitle}</p>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,269 @@
-import Image from 'next/image'
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillGoogleCircle,
+  AiFillHtml5
+} from 'react-icons/ai';
+import Image from "next/image";
+import dean from '../../public/dean.jpg';
+import Card from './cards';
+
+import { FaCode, FaCodeBranch, FaViber, FaRProject, FaCss3Alt } from 'react-icons/fa';
+import { DiPython } from 'react-icons/di';
+import { SiAngularjs, SiPowerapps, SiPowerautomate, SiJavascript, SiReact, SiCsharp, SiAdobephotoshop, SiAdobepremierepro, SiCanva, SiGmail, SiTableau, SiMui, SiPowerbi, SiMysql, SiMicrosoftsqlserver } from 'react-icons/si';
+import { TbBrandNextjs, TbBrandOffice, TbBrandTailwind } from 'react-icons/tb';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className='bg-white px-10 text-black'>
+     <section className='min-h-screen'>
+      <nav className='py-10 mb-12 flex justify-between'>
+        <h1 className='text-lg font-bold'>dean</h1>
+        <ul className='flex items-center'>
+          <li>
+            <BsFillMoonStarsFill />
+          </li>
+          <li>
+            <a className='bg-gradient-to-r from-orange-400 to-rose-400 text-white px-4 py-2 rounded-md ml-8' href='https://drive.google.com/file/d/1uiPkICXh5K2d4reRxGUmKoZleeCS0_L8' target="_blank">
+            Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+      <div className='text-center p-10'>
+        <h2 className='text-5xl py-2 font-bold text-orange-400'>Deanna Jeanne Caloracan</h2>
+        <h3 className='text-2xl py-2'>Computer Engineer</h3>
+        <p className='text-md py-5 leading-8 text-gray-800'>
+          Lagi&apos;t lagi para sa Pilipino.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      
+      <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 hover:font-bold'>
+      <a href="https://www.linkedin.com/in/deancaloracan/">
+        <AiFillLinkedin />
+        </a>
+      <a href="https://github.com/deancaloracan">
+        <AiFillGithub />
+        </a>
+        <a href="viber://add?number=639279530040">
+        <FaViber />
+        </a>
+        <a href="mailto:dean.caloracan@gmail.com">
+        <SiGmail />
+        </a>
+      </div>
+
+      <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={dean} alt="deancaloracan"/>
+            </div>
+      </div>
+      </div>
+     </section>
+
+
+     <section className='p-10 bg-gradient-to-r from-orange-400 to-rose-400'>
+
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white font-bold">Services I can offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            Since the beginning of my journey as a developer and data analyst, I have done work for government agencies, for startups and collaborated with many leaders and innovators to create digital products  and solutions.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            I offer from a wide range of services, including project management, data  management and analysis, programming and teaching.
+            </p>
+          </div>
+
+            <section className='py-4'>
+        <h3 className="text-3xl py-1 dark:text-white font-bold">Tools I Know and Use</h3>
+        <div className="grid grid-cols-2 gap-4 dark:text-white">
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Data Visualization and BI tools</h3>
+            <ul>
+              <li>Microsoft Power BI</li>
+              <li>Tableau</li>
+            </ul>
+            <div className="flex gap-2">
+            <SiPowerbi className="text-4xl mt-4" />
+            <SiTableau className="text-4xl mt-4" />
+            </div>
+          </div>
+
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Database Administration</h3>
+            <ul>
+              <li>SQL (MySQL, MSSQL)</li>
+              <li>NoSQL (MongoDB, Firestore)</li>
+            </ul>
+            <div className="flex gap-2">
+            <SiMysql className="text-4xl mt-4" />
+            <SiMicrosoftsqlserver className="text-4xl mt-4" />
+            </div>
+          </div>
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Programming Languages</h3>
+            <ul>
+              <li>Python</li>
+              <li>R</li>
+              <li>C#</li>
+              <li>C++</li>
+              <li>Bash Scripting</li>
+            </ul>
+            <div className="flex gap-2">
+              <DiPython className="text-4xl mt-4" />
+              <FaRProject className="text-4xl mt-4" />
+              <SiCsharp className="text-4xl mt-4" />
+              <FaCode className="text-4xl mt-4" />
+            </div>
+          </div>
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Web Development and Design</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JS</li>
+              <li>PHP</li>
+            </ul>
+            <div className="flex gap-2">
+              <AiFillHtml5 className="text-4xl mt-4" />
+              <FaCss3Alt className="text-4xl mt-4" />
+              <SiJavascript className="text-4xl mt-4" />
+
+            </div>
+            
+          </div>
+
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >UI Frameworks</h3>
+            <ul>
+              <li>Next.js</li>
+              <li>AngularJS</li>
+              <li>ReactJS</li>
+            </ul>
+            <div className="flex gap-2">
+              <TbBrandNextjs className="text-4xl mt-4" />
+              <SiAngularjs className="text-4xl mt-4" />
+              <SiReact className="text-4xl mt-4" />
+            </div>
+          </div>
+
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Styling</h3>
+            <ul>
+              <li>TailwindCSS</li>
+              <li>MUI</li>
+            </ul>
+            <div className="flex gap-2">
+            <TbBrandTailwind className="text-4xl mt-4" />
+            <SiMui className="text-4xl mt-4" />
+            </div>
+          </div>
+
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Productivity Tools</h3>
+            <ul>
+              <li>Microsoft Office</li>
+              <li>Google Workspace Applications</li>
+              <li>Canva</li>
+            </ul>
+            <div className="flex gap-2">
+            <SiCanva className="text-4xl mt-4" />
+            <TbBrandOffice className="text-4xl mt-4" />
+            <AiFillGoogleCircle className="text-4xl mt-4" />
+            </div>
+          </div>
+
+          <div> 
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Automation Tools</h3>
+            <ul>
+              <li>Apps Script</li>
+              <li>Microsoft PowerApps</li>
+              <li>Power Automate</li>
+            </ul>
+            <div className="flex gap-2">
+            <SiPowerapps className="text-4xl mt-4" />
+            <SiPowerautomate className="text-4xl mt-4" />
+              </div>
+
+          </div>
+
+          <div>
+            <h3 className="text-xl py-1 dark:text-white font-bold" >Multimedia Tools</h3>
+            <ul>
+              <li>Adobe Photoshop</li>
+              <li>Adobe Premiere</li>
+            </ul>
+            <div className="flex gap-2">
+              <SiAdobephotoshop className="text-4xl mt-4" />
+              <SiAdobepremierepro className="text-4xl mt-4" />
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className='py-4'>
+          <h3 className="text-3xl py-3 dark:text-white font-bold">Projects I Managed</h3>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3">
+          <Card
+        imageSrc="/chemistry.svg"
+        title="UP Kapnayan"
+        subtitle="Card Subtitle"
+        link="#"
+      />
+          <Card
+        imageSrc="/cc.jpg"
+        title="Coding Challenge"
+        subtitle="Card Subtitle"
+        link="#"
+      />
+      <Card
+        imageSrc="/dapo.jpg"
+        title="Project DAPO"
+        subtitle="Card Subtitle"
+        link="https://apkcombo.com/dapo-data-analytics-on-patho/com.vlct.dapo_app/"
+      />
+      <Card
+        imageSrc="/enroll.jpg"
+        title="Stack Enroll"
+        subtitle="Card Subtitle"
+        link="https://enroll.xsn.edu.ph/"
+      />
+      <Card
+        imageSrc="/ids.jpg"
+        title="Stack ID System"
+        subtitle="Card Subtitle"
+        link="#"
+      />
+      <Card
+        imageSrc="/gradebook.jpg"
+        title="Stack Gradebook"
+        subtitle="Card Subtitle"
+        link="https://gradebook.xsn.edu.ph"
+      />
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <h3 className="text-3xl py-4 dark:text-white font-bold">Projects I Developed</h3>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <Card
+        imageSrc="/ireach.jpg"
+        title="iReach"
+        subtitle="Card Subtitle"
+        link="https://institutional-repository-8251c.web.app/"
+      />
+      <Card
+        imageSrc="/deetz.jpg"
+        title="Deetz"
+        subtitle="Card Subtitle"
+        link="https://www.deetz.events/"
+      />
+            </div>
+      
+      </section>
+          
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   )
 }
